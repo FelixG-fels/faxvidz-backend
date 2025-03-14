9a919d3764893932d849242a9ea17e333c826826
FelixG-fels <fgboat82@gmail.com>
2025-03-15 01:04:25 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import os
 from fastapi import FastAPI
 import requests
 
 app = FastAPI()
 
-DALL_E_API_KEY = "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"
-RUNWAYML_API_KEY = "Key_3914aec899fd440930ada481fec5d0ae961d8a4dd433b1078446aa992af2e33864397f257041008e33000b8bc1f9a47222129b6341eca02c4c71d3ac151b6d94"
-ELEVENLABS_API_KEY = "Sk_888a29c792a6885d61b5c0e3df70e75eefcf09555888d2ad"
+# Get API Keys from Environment Variables
+DALL_E_API_KEY = os.getenv("OPENAI_API_KEY")
+RUNWAYML_API_KEY = os.getenv("RUNWAYML_API_KEY")
+ELEVENLABS_API_KEY = os.getenv("ELEVENLABS_API_KEY")
 
 @app.get("/generate-image")
 def generate_image(prompt: str):
